f1659e93dbea9d6bc4c6e756b8bcb35e30f841fb
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-04-15 18:43:38 -0400

    make https default

--- src/lib/EmbedForm.svelte
+++ src/lib/EmbedForm.svelte
@@ -10,11 +10,11 @@
 
     async function submit(event) {
         console.log(withHttp(url))
-        const result = await saveLink(withHttp(url), userToken);
+        const result = await saveLink(withHttps(url), userToken);
         if (result.status) {
             newUrl = String(location.origin) + '/content/' + String(result.body.data.objectId)}
         else{
-            result = await saveLink(withHttps(url), userToken);
+            result = await saveLink(withHttp(url), userToken);
             if (result.status) {
                 newUrl = String(location.origin) + '/content/' + String(result.body.data.objectId)
             }
