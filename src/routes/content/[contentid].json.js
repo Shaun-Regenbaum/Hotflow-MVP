35ef8ddb204c3cb1f19b47732787e4dd83595a02
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-04-15 18:58:36 -0400

    did I change anything?

--- src/routes/content/[contentid].json.js
+++ src/routes/content/[contentid].json.js
@@ -1,10 +1,10 @@
-import * as api from '$lib/api.js';
+// import * as api from '$lib/api.js';
 
-export async function getLink(objectId) {
-    const retrievalId = objectId
-    const path = 'data/embeds/' + retrievalId
-    const response = await api.get(path);
-    console.log(response)
-    let responseStatus = response && response.status === 200 && response.statusText === 'OK';
-	return { status: responseStatus, body: await response };
-}
+// export async function getLink(objectId) {
+//     const retrievalId = objectId
+//     const path = 'data/embeds/' + retrievalId
+//     const response = await api.get(path);
+//     console.log(response)
+//     let responseStatus = response && response.status === 200 && response.statusText === 'OK';
+// 	return { status: responseStatus, body: await response };
+// }
