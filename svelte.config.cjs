89ea8314dda3773eda9984124fd22b92372e3336
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-04-26 11:11:00 -0400

    moved vite to its own config file

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -3,7 +3,7 @@ const netlify = require('@sveltejs/adapter-netlify');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-
+	preprocess: sveltePreprocess,
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
@@ -12,11 +12,5 @@ module.exports = {
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
-		vite: {
-			ssr: {
-				noExternal: ['node-fetch', '@paypal/paypal-js']
-			}
-		}
 	}
 };
