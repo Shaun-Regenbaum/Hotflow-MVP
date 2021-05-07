904bbb32822e9e44de8999dc69acb5de4ef81687
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-07 13:35:24 -0400

    menu is coming along

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -39,23 +39,20 @@
 
 </script>
 
-<div id="menu" style="width:{minimized ? '3%' : '30%'}; border-radius: {minimized ?  '7px 7px 0 0': '30px 30px 0 0'};">
-	<section on:click="{minimize}" style="height: {minimized ? '1rem' : 'inherit'};">
+<div id="menu" style="width:{minimized ? '10rem' : '30%'}; border-radius: {minimized ?  '7px 7px 0 0': '30px 30px 0 0'};">
+	<section id="toolbar" on:click="{minimize}" style="height: {minimized ? '1rem' : 'inherit'}; width: {minimized ? '5rem' : ''}; ">
 		<!-- Right now we are essentially doing fancy css stuff to make an arrow, we may want to simplify that -->
 		<button id='minimize' style='transform: rotate({minimized ? '225deg' : '45deg'});'></button>
 	</section>
-	<section style="display:{visible}">
-		<div id="card">
-			<svelte:component this={current_component.component} />
-		</div>
-	</section>
-	<section style="display:{visible}">
-		<nav>
-			{#each components as component}
-				<li style="display: inline;"><button class="component" on:click={switchComponent(component.name)}>{component.name}</button></li>
-			{/each}
-		</nav>
-	</section>
+	<div id="card" style="display:{visible}">
+		<svelte:component this={current_component.component} />
+	</div>
+	<nav style="display:{visible}">
+		{#each components as component}
+		<li style="display: inline;"><button on:click={switchComponent(component.name)}>{component.name}</button></li>
+		{/each}
+	</nav>
+
 </div>
 
 <style>
@@ -65,24 +62,31 @@
 		z-index: 2;
 		bottom: 0;
 		right: 10%;
-		padding-bottom: 20px;
 
 		/* Grid Layout: */
 		display: grid;
-		grid-template-rows: 10% 70% 10%;
+		grid-template-rows: 2rem 1fr 1rem 3rem 1rem;
+		grid-template-columns: 2rem 1fr 2rem;
 
 		/* Size of Container: */
-		max-width: 50%;
 		max-height: 50vh;
 
 		/* Colors: */
 		background-color: #e0e0e0; /* Fallback for older browsers */
 
 		/* Nueromorphism: */
-		box-shadow:  -5px -5px 20px #bebebe,
-             5px 5px 15px #ffffff;
+		box-shadow:  -5px -5px 20px #00000021,
+             5px 5px 15px #ffffff21;
 	}
-	#minimize {
+	#toolbar{
+		/*Grid Placement:*/
+		grid-column-start: 2;
+		grid-column-end: 3;
+		grid-row-start: 1;
+		grid-row-end: 2;
+	}
+
+	#minimize {  /*Probably want to redo this guy*/
 		/* Centering the Box/Arrow: */
 		margin-left: 50%;
 
@@ -91,59 +95,54 @@
   		width: 1rem;
 
 		/* Color of Box/Arrow: */
-  		border-color:rgba(0, 213, 255, 0.527);
 		background-color: rgba(246, 185, 185, 0.404);
 
 		/* Transforming the border of a box into an arrow */
   		border-width: 0px 3px 3px 0px;
 
 		/* Padding, to make it not rest on top on minimied: */
-		padding-top: 0.3rem;
 
-			border: 0;
-		padding: 0.2rem 0.5rem 0.2rem 0.5rem;
+		border: 0;
 		border-radius: 10px;
 		box-shadow: 3px 3px 10px #bebebe,
              -3px -3px 10px #ffffff;
 	}
 
 	#card {
+		/*Grid Placement:*/
+		grid-column-start: 2;
+		grid-column-end: 3;
+		grid-row-start: 2;
+		grid-row-end: 3;
 		/* Positioning: */
 		overflow-y: auto;
-		max-height: 20vh;
-		/* To give padding: */
-		padding: 1rem 1rem 1rem 1rem;
+		max-height: 40vh;
 	}
 
 	nav {
+		/*Grid Placement:*/
+		grid-column-start: 2;
+		grid-column-end: 3;
+		grid-row-start: 4;
+
 		/* To make menu items evely spaced: */
 		display: flex;
-		align-items: stretch;
+		align-items: center;
 		justify-content: space-around;
 
-		/* To make it always stay nicely on bottom:*/
-		max-height: 3rem;
-
 		/* Shape of menu bar: */
-		border-radius: 4rem/1.3rem;
+		border-radius: 1rem/2rem;
 
 		/* Nueuromorphism: */
-		padding: 10px 0 10px 0;
-		margin: 5px 1rem 5px 1rem;
-		background: linear-gradient(145deg, #cacaca, #f0f0f0);
-		box-shadow:   inset 2px 2px 10px #a7a7a773,
-			inset -3px -3px 10px #ffebeb73,
-            10px 10px 10px #ffffffb2,
-			inset -20px -20px 130px #ebebebb2;
+		background: #e2e2e2f3;
+		box-shadow: 3px 5px 5px #ffffff,
+		 -3px -3px 10px #9c9c9c,
+		  inset 1px 5px 20px #b3b0b0,
+		 inset -1px -10px 30px #e9e9e9;
 
 	}
 
-	section{
-		/* This is to give padding between all the sections */
-		padding-top: 1%;
-	}
-
-	.component {
+	button {
 		/* Removing all the default outlines: */
 		border: 0;
   		outline:0;
@@ -151,7 +150,7 @@
 		padding: 0.2rem 0.5rem 0.2rem 0.5rem;
 		color:rgba(65, 65, 65, 0.719);
 		border-radius: 10px;
-		box-shadow: 3px 3px 10px #bebebe,
-             -3px -3px 10px #ffffff;
+		box-shadow: 3px 3px 8px #bebebe,
+             -3px -3px 8px #ffffff;
 	}
 </style>
