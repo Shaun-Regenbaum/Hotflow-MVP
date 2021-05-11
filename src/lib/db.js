8364a6cbf1d4d440ff32fbf37a4d8dbc20381b66
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-10 22:06:37 -0400

    finally working on netlify, was an npm issue

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
 
+
 const url = import.meta.env.VITE_SUPABASE_URL;
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+console.log(url, key)
+
 const supabase = createClient(String(url)
   , String(key) 
 )
