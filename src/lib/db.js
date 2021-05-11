314c5651d8612cfdacb5a2a1ef30ca42e7fb62dd
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-10 22:01:58 -0400

    lets try it again

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(String(url)
+  , String(key) 
 )
 
 export default supabase
