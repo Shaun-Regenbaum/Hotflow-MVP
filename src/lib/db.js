9af36c9117fc087d5b9476f7355c88ac0d9e32ad
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-10 21:49:24 -0400

    no?

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
 const url = import.meta.env.VITE_SUPABASE_URL;
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY
@@ -6,4 +6,4 @@ const key = import.meta.env.VITE_SUPABASE_ANON_KEY
 // @ts-ignore
 const supabase = createClient(url, key );
 
-export default supabase;
+export default supabase
