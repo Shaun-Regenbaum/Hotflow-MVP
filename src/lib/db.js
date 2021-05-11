27ad68e0357a776af155c8c6a6139061fd4ca03e
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-10 20:52:53 -0400

    now?

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-	supabaseUrl = import.meta.env.VITE_SUPABASE_URL,
-	supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+const url = import.meta.env.VITE_SUPABASE_URL;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// @ts-ignore
+const supabase = createClient(url, key );
 
 export default supabase;
