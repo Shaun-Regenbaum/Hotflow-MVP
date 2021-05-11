8a93e488c6cbcfc97cf9ee3ca9ddd372cab309b2
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-05-10 22:08:11 -0400

    stopped using env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js'
 
 
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const url = "https://xrrsefhfqueyquewoqym.supabase.co";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDM0MDMwMSwiZXhwIjoxOTM1OTE2MzAxfQ.llywRLnofuzJuSmobRrSco7Z8zV6ws32PB7oqsqOoVg";
 console.log(url, key)
 
 const supabase = createClient(String(url)
